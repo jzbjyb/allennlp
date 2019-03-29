71cee1dec61ad0e5ec7132d50f673134a73ae598
jzb <zhengbaj@cs.cmu.edu>
2019-03-29 12:01:25 -0400

    add more epochs to retag

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@
 
 config=$1
 out_dir=$2
+args="${@:3}"
 
-allennlp train ${config} --serialization-dir ${out_dir} --include-package multitask
+allennlp train ${config} --serialization-dir ${out_dir} --include-package multitask ${args}
